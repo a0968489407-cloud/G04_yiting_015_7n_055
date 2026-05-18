23081ddcdd467aab739df22c3cb64d70d36a9cc4
7nnn <13579shelly4456.com@gmail.com>
2026-05-18 23:33:36 +0800

    新增撞擊，減速，加速，加命，分裂音效，優化小球bug

--- survivor/src/Game.java
+++ survivor/src/Game.java
@@ -2,6 +2,8 @@ import javax.swing.JFrame;
 
 public class Game {
     public static void main(String[] args) {
+        // 在遊戲一開始，畫面還沒出來前先載入音效
+        SoundManager.init();
         JFrame frame = new JFrame("Ball Survival Game");
         Display display = new Display(1200, 720);
         
